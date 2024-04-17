d7dd05d60526f9b99f8d5918abfeda6400f59f79
Casondra Simons <casondra.simons@smusd90.org>
2024-04-17 07:23:59 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main{
     System.out.print("shine");
   }
   public static void summer (int temp) {
-    if(temp > 80) {
+    if(temp > 100) { // changed temp
       System.out.print("*");
       temp -= 1;
       summer (temp);
